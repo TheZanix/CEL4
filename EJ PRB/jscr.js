f28dca9760f0964d3d7f48afd601d062b4b78512
ElZanix <eliasfahuas@liceovvh.cl>
2024-04-18 14:24:55 -0400

    ok

---
+++ EJ PRB/jscr.js
@@ -0,0 +1,13 @@
+function noNegative(arr) {
+    for (let i = 0; i < arr.length; i++) {
+        if (arr[i] < 0) {
+            arr[i] = 0;
+        }
+    }
+    return arr;
+}
+
+let input = prompt("Ingrese números");
+let array = input.split(",").map(Number);
+let resultArray = noNegative(array);
+let resultElement = document.getElementById("result").innerHTML = "Result: "(", ");
