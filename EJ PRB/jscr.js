db235632885fb114077b95dd258e226950821de3
ElZanix <eliasfahuas@liceovvh.cl>
2024-05-23 11:13:27 -0400

    OK

--- EJ PRB/jscr.js
+++ EJ PRB/jscr.js
@@ -1,13 +1,14 @@
 function noNegative(arr) {
-    for (let i = 0; i < arr.length; i++) {
-        if (arr[i] < 0) {
-            arr[i] = 0;
-        }
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] < 0) {
+      arr[i] = 0;
     }
-    return arr;
+  }
+  return arr;
 }
 
 let input = prompt("Ingrese números");
 let array = input.split(",").map(Number);
 let resultArray = noNegative(array);
-let resultElement = document.getElementById("result").innerHTML = "Result: "(", ");
+let resultElement = document.getElementById("result");
+resultElement.innerHTML = "Result: " + resultArray.join(", ");
