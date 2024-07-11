ec70469a8b6dd6510eb5a8e861b751bf891991c2
ElZanix <eliasfahuas@liceovvh.cl>
2024-07-11 09:07:18 -0400

    OK

---
+++ BUCLES/dasda/script.js
@@ -0,0 +1,11 @@
+function onoff(elemento) {
+    console.log("elemento clickeado", elemento);
+    elemento.style.backgroundColor = 'red'; // Cambia el color de fondo del botón
+    elemento.textContent = 'off'; // Cambia el texto del botón
+}
+
+function onoff(elemento) {
+    console.log("elemento clickeado", elemento);
+    elemento.style.backgroundColor = 'green'; // Cambia el color de fondo del botón
+    elemento.textContent = 'on'; // Cambia el texto del botón
+}
